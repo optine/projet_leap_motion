aa5494d8cc0d242fb026411bba72ce18e9feee67
karim1412 <coulibaly91karim@gmail.com>
2016-12-14 17:13:53 +0100

    suite mouvement et automate

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,8 @@
 // -------------------------------
 
 var game = new Phaser.Game(
-  800,
-  600,
+  innerWidth,
+  850,
   Phaser.CANVAS,
   'montgolfiere',
   {
@@ -15,6 +15,9 @@ var game = new Phaser.Game(
   }
 );
 var player;
+var item;
+var enemy;
+var time;
 var cursors;
 
 // ---------
@@ -25,37 +28,48 @@ function preload()
 {
   game.load.image('background','images/blue-sky.jpg');
   game.load.image('balloon', 'images/HotAirBalloon.png');
+  game.load.image('bulle', 'images/helium.png')
+  game.load.image('ennemi', 'images/oiseau.png' )
 }
 
 
 function create()
 {
   // Déclaration de la taille du world (800x6000 pixels)
-  game.add.tileSprite(0, 0, 800, 20000, 'background');
+  game.add.tileSprite(0, 0, innerWidth, 20000, 'background');
 
-  game.world.setBounds(0, 0, 800, 20000);
+  game.world.setBounds(0, 0, innerWidth, 20000);
 
   // Démarrage du système de Physics : ARCADE
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  game.physics.arcade.gravity.y = -10;
+  game.physics.arcade.gravity.y = 0;
 
   // Création des curseur pour le controle clavier
   cursors = game.input.keyboard.createCursorKeys();
   
   // Ajout du ballon
   player = game.add.sprite(100, 300, 'balloon');
+  item = game.add.sprite(game.world.upX, 100, 100, 'bulle');
   // Activation de la gravité sur le player
   game.physics.arcade.enable(player);
   player.body.collideWorldBounds = true;
   player.y = game.world.height;
+  enemy = game.add.physicsGroup();
+  var tween = game.add.tween(player).to( { x: 1000 }, 5000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+  var enemy = game.create(Math.random() * (innerWidth - 100) + 100, 0, 'ennemi');
+  enemy.body.velocity.x = game.rnd.between(100, 200);
+  y += 48;
+
 }
 
 function update()
 {
-
+  player.body.velocity.y = -100;
+  game.physics.arcade.overlap(player, enemy, null, this);
+  enemy.forEach(checkPos, this);
 /*  if (cursors.up.isDown)
   {
-    player.body.velocity.y = -300;
+    player.body.velocity.y = MATH PI/8;
   }
   else if (cursors.down.isDown)
   {
@@ -63,6 +77,15 @@ function update()
   }*/
 
   game.camera.follow(player);
+}
+
+function checkPos (enemy) {
+
+    if (enemy.x > 800)
+    {
+        enemy.x = -100;
+    }
+
 }
 
 function render()
